4ae7947d55753a2615f4d9b61d4a07eaf302f0d4
Chiuliana <chichioiuliana@gmail.com>
2024-10-31 08:10:41 +0200

    Implement Singleton pattern for Library class

---
+++ Labs/Lab_1/singleton/library_singleton.py
@@ -0,0 +1,20 @@
+class Library:
+     # Rrepresents the library managing books and users
+
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Library, cls).__new__(cls)
+            cls._instance.books = []
+            cls._instance.users = []
+        return cls._instance
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def add_user(self, user):
+        self.users.append(user)
+
+    def __str__(self):
+        return f"Library has {len(self.books)} books and {len(self.users)} users."
