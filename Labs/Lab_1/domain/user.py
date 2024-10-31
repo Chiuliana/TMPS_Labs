6cafcdd6c0acf326156aba41f7bbcaf54d0d0645
Chiuliana <chichioiuliana@gmail.com>
2024-10-31 08:03:21 +0200

    Add core classes: Book and User

---
+++ Labs/Lab_1/domain/user.py
@@ -0,0 +1,9 @@
+class User:
+    # Represents a library user with a name and user ID
+
+    def __init__(self, name, user_id):
+        self.name = name
+        self.user_id = user_id
+
+    def __str__(self):
+        return f"User {self.name} (ID: {self.user_id})"
